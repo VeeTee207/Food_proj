bae9ef2e5b11c3972fbb608019289bb0bbb273f9
veetee207 <v.terehov207@gmail.com>
2021-08-08 17:46:07 -0400

    3. activate sales end timer

--- src/js/script.js
+++ src/js/script.js
@@ -24,8 +24,10 @@ window.addEventListener('DOMContentLoaded', () =>{
         tabContent[i].classList.remove('hide');
         tabs[i].classList.add('tabheader__item_active');
     }
+
     hideTabContent();
     showTabContent();
+
     tabsParent.addEventListener('click', (event) => {
         const target = event.target;
 
@@ -38,7 +40,68 @@ window.addEventListener('DOMContentLoaded', () =>{
                 }
             });
         }
-
     });
+
+    // Timer ----- sales counter
+
+    const deadline = '2021-08-15'; // set at tth day from now
+
+    function getTimeRemaining(endtime) {
+        const t = Date.parse(endtime) - Date.parse(new Date()),
+        // reminder -- 1 sec is 1000 mSec
+            days = Math.floor( (t/(1000*60*60*24)) ),
+            seconds = Math.floor( (t/1000) % 60 ),
+            minutes = Math.floor( (t/1000/60) % 60 ),
+            hours = Math.floor( (t/(1000*60*60) % 24) );
+
+        return {
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    }
+    // add  0 in front of number
+    function getZero(num){
+        if (num >= 0 && num < 10) { 
+            return '0' + num;
+        } else {
+            return num;
+        }
+    }
+
+    // set counter on screen
+    function setClock(selector, endtime) {
+
+        const timer = document.querySelector(selector),
+            days = timer.querySelector("#days"),
+            hours = timer.querySelector('#hours'),
+            minutes = timer.querySelector('#minutes'),
+            seconds = timer.querySelector('#seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+        updateClock();  // elemonate screen blinkink while loading page
+
+        function updateClock() {
+            const t = getTimeRemaining(endtime);
+            days.textContent = getZero(t.days);
+            hours.textContent = getZero(t.hours);
+            minutes.textContent = getZero(t.minutes);
+            seconds.textContent = getZero(t.seconds);
+            
+            // either way is ok  --- textContent  or innerHTML
+            // days.innerHTML = getZero(t.days);
+            // hours.innerHTML = getZero(t.hours);
+            // minutes.innerHTML = getZero(t.minutes);
+            // seconds.innerHTML = getZero(t.seconds);
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+            }
+        }
+    }
+
+    setClock('.timer', deadline);
 });
 
