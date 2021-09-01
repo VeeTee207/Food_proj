3bd6ec9a2c853698d6e7bfd6c1d79f31745268b9
veetee207 <v.terehov207@gmail.com>
2021-08-31 23:26:48 -0400

    10. Added temp db.json file as as data from backEnd and test it with json-server

--- src/js/script.js
+++ src/js/script.js
@@ -129,45 +129,52 @@ window.addEventListener('DOMContentLoaded', () =>{
                 <div class="menu__item-divider"></div>
                 <div class="menu__item-price">
                     <div class="menu__item-cost">Цена:</div>
-                    <div class="menu__item-total"><span>${this.price}</span> $/день</div>
+                    <div class="menu__item-total"><span>${this.price}</span> BYR/день</div>
                 </div>
             `;
             this.parent.append(element);
         }
     }
 
-    // substitute html coding + css styling to js code for menu cards 
-    new MenuCard(
-        "img/tabs/vegy.jpg",
-        "vegy",
-        'Меню "Фитнес"',
-        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
-        9,
-        ".menu .container",
-        // 'menu__item',
-        // 'big',
-    ).render();
+    const getResource = async (url) => {
+        const res = await fetch(url)
+            if (!res.ok) {
+                throw new Error(`Could not fetch ${url}, ststus: ${res.status}`);
 
-// just create object new MenuCard without referal to any variable -- another options for object creation
-    new MenuCard(
-        "img/tabs/post.jpg",
-        "post",
-        'Меню "Постное"',
-        'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
-        14,
-        ".menu .container",
-        // 'menu__item',
-    ).render();
+            }
+        return await res.json();
+    };
+        //options 1
+    getResource('http://localhost:3000/menu')
+    .then(data => {
+        data.forEach(({img, altimg, title, descr, price}) => {
+            new MenuCard(img, altimg, title, descr, price, ".menu .container").render();
+        });
+    });
 
-    new MenuCard(
-        "img/tabs/elite.jpg",
-        "elite",
-        'Меню “Премиум”',
-        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
-        21,
-        ".menu .container",
-    ).render();
+    
+    // options 2
+    // getResource('http://localhost:3000/menu')
+    // .then(data => {createCard(data)});
 
+    // function createCard(data) {
+    //     data.forEach(({img, altimg, title, descr, price}) => {
+    //         const element = document.createElement('div');
+    //         price = price *2.5 //  2.5 currncy rate
+    //         element.classList.add('menu__item');
+    //         element.innerHTML = `
+    //             <img src=${img} alt=${altimg}>
+    //             <h3 class="menu__item-subtitle">${title}</h3>
+    //             <div class="menu__item-descr">${descr}</div>
+    //             <div class="menu__item-divider"></div>
+    //             <div class="menu__item-price">
+    //                 <div class="menu__item-cost">Цена:</div>
+    //                 <div class="menu__item-total"><span>${price}</span> BYR/день</div>
+    //             </div>
+    //         `;
+    //         document.querySelector('.menu .container ').append(element);
+    //     });
+    // }
 
 
    // Modal window to buttons Связаться с нами
@@ -225,18 +232,30 @@ window.addEventListener('DOMContentLoaded', () =>{
        // forms
        const forms = document.querySelectorAll('form');
        const message = {
-           loading: 'img/form/spinner.svg',
-        //    loading: 'Loading ...',
-   
+           loading: 'img/form/spinner.svg',   
            success: 'Thanks! We will contact you',
            failure: 'Something goes wrong ...',
        };
        forms.forEach(item => {
-           postData(item);
+            bindPostData(item);
        });
-   
-       // we use 2 formats - 1)formData, 2) JSON. The type of format depends on format needed for backEnd
-       function postData(form) {
+
+       // dont know when server return code so use async
+       // await -- means that i wait till get result
+        // since json-additional method -- get await
+        const postData = async (url, data) => {
+            let res = await fetch(url, {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: data
+            });
+        
+            return await res.json();
+        };
+
+       function  bindPostData(form) {
            form.addEventListener('submit', (e) => {
                e.preventDefault();
                 // spinnner
@@ -251,19 +270,9 @@ window.addEventListener('DOMContentLoaded', () =>{
                const formData = new FormData(form);
    
             //    working with JSON data now
-               const object = {};
-               formData.forEach(function(value, key){
-                   object[key] = value;
-               });
+                const json = JSON.stringify(Object.fromEntries(formData.entries()));
    
-               fetch('server.php', {
-                    method: "POST",
-                    headers: {
-                        'Content-type': 'apllcatiom/json',
-                    },
-                    body: JSON.stringify(object),
-                })
-                .then(data => data.text())
+                postData('http://localhost:3000/requests', json)
                 .then(data => {
                     console.log(data);
                     showThanksModal(message.success);
@@ -300,7 +309,11 @@ window.addEventListener('DOMContentLoaded', () =>{
         }, 4000);
     }
 
+    fetch('http://localhost:3000/menu')
+    .then(data => data.json())
+    .then(res => console.log(res));
 });
+
 
 
 
